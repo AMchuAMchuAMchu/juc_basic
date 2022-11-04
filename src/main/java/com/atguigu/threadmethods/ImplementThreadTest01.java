c7a69547a61bd6cf4839cdce73d78f453aa0a0c4
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-04 19:31:56 +0800

    shino_hecate

--- src/main/java/com/atguigu/threadmethods/ImplementThreadTest01.java
+++ src/main/java/com/atguigu/threadmethods/ImplementThreadTest01.java
@@ -2,6 +2,10 @@ package com.atguigu.threadmethods;
 
 import com.sun.javafx.geom.transform.Translate2D;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * Description ==> TODO
  * BelongsProject ==> juc_basic
@@ -11,15 +15,31 @@ import com.sun.javafx.geom.transform.Translate2D;
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
 public class ImplementThreadTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 //        T11 t11 = new T11();
 //        t11.start();
 
-        T2 t2 = new T2();
-        Thread thread = new Thread(t2);
+//        T2 t2 = new T2();
+//        Thread thread = new Thread(t2);
+//        thread.start();
+
+        T3 t3 = new T3();
+        FutureTask<String> futureTask = new FutureTask<>(t3);
+        String s = futureTask.get();
+        Thread thread = new Thread(futureTask);
         thread.start();
+        System.out.println(" >> "+s);
 
 
+    }
+}
+
+class T3 implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("T3...");
+        return "callable...";
     }
 }
 
